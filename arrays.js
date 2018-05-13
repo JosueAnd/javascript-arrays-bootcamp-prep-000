b39720afcfd73e8d1e29e6c26d2e1fac76802d49
Joshua Andujar <andujar0218@gmail.com>
2018-05-13 03:36:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,11 +4,15 @@ var chocolateBars =
   'kitkat',
   'skittles'];
   
-  function addElementToBeginningOfArray(array, element){
+  function addElementToBeginningOfArray(array, element) {
     return [element, ...array];
   }
   
   function destructivelyAddElementToBeginningOfArray(array, element){
     array.unshift (element);
+    return array;
+  }
+  function addElementToEndOfArray(array, element) {
+    array.push (element);
     return array;
   }
