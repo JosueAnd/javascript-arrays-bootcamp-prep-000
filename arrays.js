5a448a83e72dad3744508d4250cc50ee3917777a
Joshua Andujar <andujar0218@gmail.com>
2018-05-13 03:56:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,4 +35,10 @@ var chocolateBars =
     return array.slice(1);
   }
   
-  
+  function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop;
+    return array;
+  }
+  function removeElementFromEndOfArray(array) {
+    return array.length - 1;
+  }
